05d2b6f962b3b7d0a9bc43446ed514db5cad718b
imonweb <contact@imonweb.co.uk>
2022-03-21 10:05:22 +0000

    React Social Login with Passport

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+  const logout = () => {
+    window.open("http://localhost:5000/auth/logout", "_self")
+  }
+
   return (
     <div className="navbar">
-      <span>Imon App</span>
-      <ul className="list">
-        <li className="listItem">
-          <img src="https://images.pexels.com/photos/3112898/pexels-photo-3112898.jpeg?cs=srgb&dl=pexels-disha-sheta-3112898.jpg&fm=jpg" alt="" className="avatar" />
-        </li>
-        <li className="listItem">John Doe</li>
-        <li className="listItem">Logout</li>
-      </ul>
+      <span className="logo">
+        <Link className="link" to="/">
+          Imon App
+        </Link>
+      </span>
+      {user ? (
+        <ul className="list">
+          <li className="listItem">
+            {/* <img src="https://images.pexels.com/photos/3112898/pexels-photo-3112898.jpeg?cs=srgb&dl=pexels-disha-sheta-3112898.jpg&fm=jpg" alt="" className="avatar" /> */}
+            <img src={user.photos[0].value} alt="" className="avatar" />
+          </li>
+          <li className="listItem">{user.displayName}</li>
+          <li className="listItem" onClick={logout}>
+            Logout
+          </li>
+        </ul>
+      ) : (
+        <Link className="link" to="login">
+          Login
+        </Link>
+      )}
     </div>
   )
 }
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import { posts } from "../data";
 
 const Post = () => {
 
-  const post = posts[2];
+  // const post = posts[2];
+  const location = useLocation()
+  const path = location.pathname.split("/")[2]
+
+  const post = posts.find(p=>p.id.toString() === path)
+
+  console.log(location)
+
   return (
     <div className='post'>
       <img src={post.img} className="postImg" />
